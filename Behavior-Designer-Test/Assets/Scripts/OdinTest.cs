ab42d334fdd001613854f0b0c7cc2da964f83f89
lizijun <1655753014@qq.com>
2018-08-27 22:16:46 +0800

    导入Behavior Designer和Odin，使用正常

---
+++ Behavior-Designer-Test/Assets/Scripts/OdinTest.cs
@@ -0,0 +1,22 @@
+﻿using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OdinTest : MonoBehaviour
+{
+    [MinValue(5)]
+    public int age;
+    public List<string> nages;
+    public Vector3 pos;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
